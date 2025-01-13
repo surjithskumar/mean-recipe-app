165b46d0899b4953ca0d3c74d8d2382885b128a5
SURJITH S KUMAR <surjithskumar23@gmail.com>
2025-01-13 12:10:30 +0530

    home page

--- client/src/app/recipe/recipe.component.ts
+++ client/src/app/recipe/recipe.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css'
 })
