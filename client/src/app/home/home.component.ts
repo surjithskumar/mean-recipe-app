1b8fc83ef07c1507811ddffa30ff6f8702ab62f5
SURJITH S KUMAR <surjithskumar23@gmail.com>
2025-01-16 12:30:49 +0530

    search functionality

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { RouterLink } from '@angular/router';
+import { ApiService } from '../services/api.service';
+
 
 @Component({
   selector: 'app-home',
@@ -11,4 +13,15 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeComponent {
 
+  homeRecipes:any=[]
+  constructor(private api:ApiService){}
+  ngOnInit(){
+    this.getAllHomeRecipes()
+  }
+  getAllHomeRecipes(){
+    this.api.getAllrecipesAPI().subscribe((res:any)=>{
+      this.homeRecipes=res.slice(0,6)
+      console.log(this.homeRecipes);
+    })
+  }
 }
--- client/src/app/recipe/recipe.component.ts
+++ client/src/app/recipe/recipe.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { ApiService } from '../services/api.service';
+import { SearchPipe } from '../pipe/search.pipe';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,SearchPipe,FormsModule],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css'
 })
 export class RecipeComponent {
 
+  allRecipes:any=[]
+  dummyRecipes:any=[]
+  searchKey:string=""
+
+  constructor(private api:ApiService){}
+
+  ngOnInit() {
+    this.getAllRecipes();
+  }
+
+  getAllRecipes(){
+    this.api.getAllrecipesAPI().subscribe((res:any)=>{
+      this.allRecipes=res
+      this.dummyRecipes=this.allRecipes
+      console.log(this.allRecipes);
+    })
+  }
+
+  filterRecipes(recipeType:string,recipeName:string){
+    this.allRecipes=this.dummyRecipes.filter((item:any)=>item[recipeType].includes(recipeName))
+  }
 }
